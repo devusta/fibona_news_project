f6350a513d14002695cbafb3bd4da4ca492367c2
Asadbek <asadbekdusmatov0399@gmail.com>
2024-07-16 21:59:01 +0900

    last changes

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ["django-mohirdev-demo.uz", "www.django-mohirdev-demo.uz", '127.0.0.1']
+ALLOWED_HOSTS = [".onrender.com", "django-mohirdev-demo.uz", "www.django-mohirdev-demo.uz", '127.0.0.1']
 
 
 # Application definition
